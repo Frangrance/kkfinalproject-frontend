d157a60c6f1a59336b0d290199ab1fb3946d94ff
Kutay Keleş <45495735+Frangrance@users.noreply.github.com>
2021-03-17 14:34:07 +0300

    17 mart

--- northwind/src/app/components/category/category.component.ts
+++ northwind/src/app/components/category/category.component.ts
@@ -32,4 +32,18 @@ export class CategoryComponent implements OnInit {
       return "list-group-item"
     }  
   }
+
+  getAllCategoryClass(){
+    if(!this.currentCategory){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
+  }
+  clearCurrentCategory(){
+    if(this.currentCategory)
+    {
+      this.currentCategory=null;
+    }
+  }
 }
